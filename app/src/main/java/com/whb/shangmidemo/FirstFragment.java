1916022f063959782e3d6e894794a942f3b304e0
wwh <you>
2023-11-02 20:00:30 +0800

    临时提交

--- app/src/main/java/com/whb/shangmidemo/FirstFragment.java
+++ app/src/main/java/com/whb/shangmidemo/FirstFragment.java
@@ -4,49 +4,85 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-
+import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.DatePicker;
-import com.applandeo.materialcalendarview.utils.CalendarProperties;
+import com.applandeo.materialcalendarview.builders.DatePickerBuilder;
+import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
 import com.whb.shangmidemo.adapter.BusinessFirstAdapter;
 import com.whb.shangmidemo.databinding.FragmentFirstBinding;
 import com.whb.shangmidemo.entity.BusinessFirstBean;
 import com.whb.shangmidemo.utils.GridSpaceItemDecoration;
+import me.jessyan.autosize.internal.CustomAdapt;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements CustomAdapt {
     
     private FragmentFirstBinding binding;
-
+    
     private BusinessFirstAdapter businessFirstAdapter;
     
     private DatePicker datePicker;
     
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         initButton();
         initIncomeDate();
         initAdapter();
-
+        initDatePicker();
+        initSpinner();
         return binding.getRoot();
     }
-
+    
+    /***
+     * 财务报表
+     * */
     private void initAdapter() {
         businessFirstAdapter = new BusinessFirstAdapter();
         binding.rvStatement.setLayoutManager(new GridLayoutManager(requireActivity(), 4));
-        binding.rvStatement.addItemDecoration(
-                new GridSpaceItemDecoration(30, 40));
+        binding.rvStatement.addItemDecoration(new GridSpaceItemDecoration(30, 40));
         binding.rvStatement.setAdapter(businessFirstAdapter);
         businessFirstAdapter.setNewInstance(initTestData());
+        
+        binding.lvPullDown.setOnClickListener(v -> {
+            if (businessFirstAdapter.getData().size() > 4) {
+                businessFirstAdapter.setNewInstance(initTestData(4));
+            } else {
+                businessFirstAdapter.setNewInstance(initTestData());
+            }
+        });
     }
-
+    
+    /***
+     * 选择列表
+     * */
+    private void initSpinner() {
+        List<String> list = new ArrayList<>();
+        list.add("今天");
+        list.add("昨天");
+        list.add("上周");
+        list.add("本周");
+        list.add("近7天");
+        list.add("过去30天");
+        list.add("本月");
+        list.add("上月");
+        list.add("自定义");
+        
+        SpinnerAdapter adapter = new ArrayAdapter<>(requireActivity(), R.layout.select_serial_item, list);
+        binding.spSelect.setAdapter(adapter);
+        binding.spSelect.setSelection(list.indexOf("今天"), true);
+    }
+    
     /**
      * 所有button事件
      */
@@ -67,13 +103,13 @@ public class FirstFragment extends Fragment {
                 view -> Toast.makeText(requireActivity(), "点击了本月", Toast.LENGTH_SHORT).show());
         binding.btnLastMonth.setOnClickListener(
                 view -> Toast.makeText(requireActivity(), "点击了上月", Toast.LENGTH_SHORT).show());
+        binding.btnPrint.setOnClickListener(
+                view -> Toast.makeText(requireActivity(), "点击了打印", Toast.LENGTH_SHORT).show());
         binding.btnCustomize.setOnClickListener(view -> {
-            initDatePicker();
-            
-//            datePicker.show();
+            datePicker.show();
         });
     }
-
+    
     /***
      * 总营业额
      * 营业收入
@@ -94,15 +130,25 @@ public class FirstFragment extends Fragment {
     }
     
     private void initDatePicker() {
-//        datePicker = new DatePicker(requireActivity());
-//        datePicker.getWheelLayout().setResetWhenLinkage(false);
-//        datePicker.setOnDatePickedListener(this);
-//        CalendarView calendarView = new CalendarView(requireActivity());
-//        calendarView.showCurrentMonthPage();
-        DatePicker picker = new DatePicker(requireActivity(), new CalendarProperties(requireActivity()));
-        picker.show();
+        OnSelectDateListener onSelectDateListener = new OnSelectDateListener() {
+            List<String> dates = new ArrayList<>();
+            
+            @Override
+            public void onSelect(List<Calendar> calendar) {
+                for (Calendar cal : calendar) {
+                    dates.add(cal.getTime().toString());
+                }
+                
+                Toast.makeText(requireActivity(), "点击了" + dates, Toast.LENGTH_SHORT).show();
+            }
+        };
+        
+        DatePickerBuilder builder = new DatePickerBuilder(requireActivity(),
+                onSelectDateListener).setPickerType(CalendarView.RANGE_PICKER);
+        
+        datePicker = builder.build();
     }
-
+    
     private List<BusinessFirstBean> initTestData() {
         List<BusinessFirstBean> list = new ArrayList<>();
         list.add(new BusinessFirstBean("支出金额(元)", "177315.17", 60.08f));
@@ -116,10 +162,29 @@ public class FirstFragment extends Fragment {
         return list;
     }
     
+    private List<BusinessFirstBean> initTestData(Integer count) {
+        List<BusinessFirstBean> list = new ArrayList<>();
+        list.add(new BusinessFirstBean("支出金额(元)", "177315.17", 60.08f));
+        list.add(new BusinessFirstBean("有效订单数(笔)", "29889", 49.25f));
+        list.add(new BusinessFirstBean("折前单均价(元)", "42.10", -3.57f));
+        list.add(new BusinessFirstBean("折后单均价(元)", "36.16", -2.20f));
+        return list;
+    }
+    
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        datePicker = null;
         binding = null;
     }
     
+    @Override
+    public boolean isBaseOnWidth() {
+        return true;
+    }
+    
+    @Override
+    public float getSizeInDp() {
+        return 1920;
+    }
 }
--- app/src/main/java/com/whb/shangmidemo/MainActivity.java
+++ app/src/main/java/com/whb/shangmidemo/MainActivity.java
@@ -1,6 +1,13 @@
 package com.whb.shangmidemo;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import cn.bingoogolapple.badgeview.BGABadgeViewHelper;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -8,13 +15,18 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.tabs.TabLayout;
 import com.whb.shangmidemo.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import me.jessyan.autosize.internal.CustomAdapt;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements CustomAdapt {
+    
     private ActivityMainBinding binding;
     
     @Override
@@ -23,12 +35,91 @@ public class MainActivity extends AppCompatActivity {
         
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        
         // 添加默认的Fragment到FrameLayout
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.nav_host_fragment_content, new FirstFragment())
-                    .commit();
+                    .add(R.id.nav_host_fragment_content, new FirstFragment()).commit();
         }
+        
+        initTab();
+        initSidebar();
+    }
+    
+    private void initSidebar() {
+        binding.bgCall.showTextBadge("99+");
+        binding.btnCallNumber.setOnClickListener(v -> {
+            Snackbar.make(v, "叫号", Snackbar.LENGTH_SHORT).show();
+        });
+        binding.ivMainIcon.setOnClickListener(v -> {
+            Snackbar.make(v, "设置", Snackbar.LENGTH_SHORT).show();
+        });
+        binding.ivUser.setOnClickListener(v -> {
+            Snackbar.make(v, "用户", Snackbar.LENGTH_SHORT).show();
+        });
+        binding.ivWallet.setOnClickListener(v -> {
+            Snackbar.make(v, "对账", Snackbar.LENGTH_SHORT).show();
+        });
+        binding.btnCommodity.setOnClickListener(v -> {
+            Snackbar.make(v, "商品", Snackbar.LENGTH_SHORT).show();
+        });
+    }
+    
+    private void initTab() {
+        List<String> tabList = tab();
+        for (int i = 0; i < tabList.size(); i++) {
+            binding.tlMain.addTab(binding.tlMain.newTab().setText(tabList.get(i)));
+        }
+        
+        ViewGroup tabs = (ViewGroup) binding.tlMain.getChildAt(0);
+        for (int i = 0; i < tabs.getChildCount() - 1; i++) {
+            View tab = tabs.getChildAt(i);
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) tab.getLayoutParams();
+            layoutParams.weight = 0;
+            layoutParams.setMarginEnd(50);
+            layoutParams.setMarginEnd(50);
+            layoutParams.width = 350;
+            tab.setLayoutParams(layoutParams);
+            binding.tlMain.requestLayout();
+        }
+        
+        binding.tlMain.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(getApplicationContext(), "点击了" + tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+            
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            
+            }
+            
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            
+            }
+        });
+    }
+    
+    @Override
+    public boolean isBaseOnWidth() {
+        return true;
+    }
+    
+    @Override
+    public float getSizeInDp() {
+        return 1920;
+    }
+    
+    private List<String> tab() {
+        List<String> tab = new ArrayList<>();
+        tab.add("营业报表");
+        tab.add("商业报表");
+        tab.add("日结");
+        tab.add("三方券");
+        tab.add("交接班");
+        tab.add("货价签");
+        tab.add("商品");
+        return tab;
     }
 }
